5ac6d2787e37dc8ce4d185fd23648a376b733afc
Otabek Kamalov <otakam86@gmail.com>
2018-07-05 19:26:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 function dwarfRollCall(dwarves) {
   var dwarves1 = [];
+<<<<<<< HEAD
   for (let i=0; i<dwarves.length; i++){
     dwarves1[i]=`${i+1}. ${dwarves[i]} `
   }
   
   return dwarves1.join("");
+=======
+  for (let i=0;i<d.length; i++){
+    dwarves1[i]=`${i+1}. ${dwarves[i]}`
+  }
+  
+  return dwarves1.join(" ")
+>>>>>>> 073e211f1b00b7c32d35fbeb1b3d21d3a5fb9324
 }
 
 function summonCaptainPlanet(planeteerCalls){
